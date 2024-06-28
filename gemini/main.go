f7be4ba4cfa764005b8dfd49f96a15cb021c8fba
kian1990 <269590936@qq.com>
2024-06-28 16:39:07 +0800

    Add files via upload

---
+++ gemini/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "os"
+    "github.com/google/generative-ai-go/genai"
+    "google.golang.org/api/option"
+)
+
+func main() {
+    ctx := context.Background()
+    client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Close()
+    model := client.GenerativeModel("gemini-1.5-pro")
+    cs := model.StartChat()
+
+    send := func(msg string) *genai.GenerateContentResponse {
+        fmt.Printf("== Me: %s\n== Model:\n", msg)
+        res, err := cs.SendMessage(ctx, genai.Text(msg))
+        if err != nil {
+            log.Fatal(err)
+        }
+        return res
+    }
+
+    res := send("你是谁")
+    if err != nil {
+        log.Fatal(err)
+    }
+    printResponse(res)
+}
+
+func printResponse(resp *genai.GenerateContentResponse) {
+    for _, cand := range resp.Candidates {
+        if cand.Content != nil {
+            for _, part := range cand.Content.Parts {
+                fmt.Println(part)
+            }
+        }
+    }
+    fmt.Println("---")
+}
