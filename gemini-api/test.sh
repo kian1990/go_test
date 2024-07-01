dd5cebcea16043c62f0cc52946dce74233fceedf
kian1990 <269590936@qq.com>
2024-07-01 17:52:08 +0800

    Update test.sh

--- gemini-api/test.sh
+++ gemini-api/test.sh
@@ -1,1 +1,1 @@
-curl -X POST http://127.0.0.1:9080/chat -d '{"message": "你是谁"}' -H "Content-Type: application/json" -H 'Authorization: your_apikey'
+curl -X POST http://127.0.0.1:9080/chat -d '{"message": "你是谁"}' -H "Content-Type: application/json"
